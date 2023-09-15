1815a21a9d09a19700e11be1a5eeade63c072d9a
Marcos Arroyo <Maarroyo@csumb.edu>
2023-09-14 17:13:50 -0700

    Adds a new inventory system
    added prefabs for
    - inventory slots
    - items
    
    added two scripts
    -DraggableItem
    -InventorySlot
    
    ====================
    Draggable Item Script is in charge of moving the
    
    InventorySlot Script is in charge of dropping it in other slots & only limiting to just one

---
+++ Assets/Scripts/Controls/DraggableItem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        // these three are in charge of taking it out of the slots so it can hover over the slots and not be hiding
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        
+        // this makes it so that you can drop the item to a slot, without this it will just teleport back
+        image.raycastTarget = false;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging");
+        
+        // records the movement of the mouse
+        transform.position = Input.mousePosition;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        //puts it back into the slot within the hierarchy
+        transform.SetParent(parentAfterDrag);
+        
+        image.raycastTarget = true;
+    }
+}
