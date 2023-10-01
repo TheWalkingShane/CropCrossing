1e9a7945c73176741fc242d028bca0083de1d697
Reece M Whitelaw <rebeliongaming@gmail.com>
2023-10-01 01:42:14 -0700

    Beautifcation
    Reworked menu visuals with new button, splash logo, and cursor. Most of which is animated.
    
    Reworked visuals of combination screen, adding a snazzy background as well as some snazzy HUDS for the combination areas.

---
+++ Assets/Scripts/Stat Scripts/StatKeeperB.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatKeeperB : MonoBehaviour
+{
+    //this entire script is solely for the purpose of keeping the stats tracked of each crop.
+    List<string> genes = new List<string>(){"AAaA", "bbbb", "cCcC", "DdDD"};
+    //String representations of gene sequences that influence the stats of the crop, A corresponds to nutrition
+    
+    //B corresponds to Firmness
+    
+    //C corresponds to yield
+    
+    //D corresponds to lifespan
+    
+    
+    //the actual stats of the crop
+    public float nutrition = 20f;
+    public float yield = 3f;
+    public float firmness = 15f;
+    public float lifespan = 5f;
+
+    public List<string> Retrieve_Genes()
+    {
+        return genes;
+    }
+    //this initial stats are completely arbitrary for the time being.
+    //monetary value is deliberately absent and will likely only be incorporated once the money exists in the first place.
+}
+
