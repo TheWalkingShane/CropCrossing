837711554edc54512a1e13718a333703588f4797
Reece M Whitelaw <rebeliongaming@gmail.com>
2023-10-02 16:51:29 -0700

    Draggable Plants
    So, heres why collision was broken and why the plants werent rendering properly.
    
    Because they were all just render atop the screen with no relation to the plants. Thats also why it worked with Marcos, because he used objects that were attached to the canvas aka the screen.
    
    SO- this removes that issues by forcing the canvas into 3d space and rendering my plants atop it no matter what, even if they have the same Z coords.
    
    Small issue with the draggables which I'm hoping Marcos can fix otherwise I will look into it deeper

--- Assets/Scripts/Plant Visuals/Core2.cs
+++ Assets/Scripts/Plant Visuals/Core2.cs
@@ -10,9 +10,20 @@ public class Core2 : MonoBehaviour
         transform.localScale = size;
         transform.localRotation = Quaternion.Euler(rotation);
         GetComponent<SpriteRenderer>().sprite = image;
-        
         connectionPoint = connection;
         GetComponent<SpriteRenderer>().color = color;
         transform.SetParent(connectionPoint.transform); // Set the connection point as the parent
+        
+        //The above function is mostly outdated and handled by Stem2 code.
+    }
+
+    public void SetCoreColor(Color CoreColor)
+    {
+        //This function shall set core color
+        SpriteRenderer[] childSpriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer childRenderer in childSpriteRenderers)
+        {
+            childRenderer.color = CoreColor;
+        }
     }
 }
